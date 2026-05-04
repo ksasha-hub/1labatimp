38e2ece4c3ecad007df42cb5ef467fd613c9bdd3
Александр <vka2@list.ru>
2026-05-04 16:00:47 +0700

    Update Home.js with new content on 2026-05-04

--- practice2/src/pages/Home.js
+++ practice2/src/pages/Home.js
@@ -1,5 +1,11 @@
-<summary table>
-<!-- Your summary table content goes here -->
+// Updated code content goes here
 
-<filters>
-<!-- Your filter content goes here -->
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Home Page!</h1>
+        </div>
+    );
+};
+
+export default Home;
